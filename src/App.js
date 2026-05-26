7f86a6a1ffd65baa1f40af1508b167e940f3b52a
ZomboyTepix <juputepix5@gmail.com>
2026-05-26 20:19:14 +0200

    feat: restore default React boilerplate content to App header

--- src/App.js
+++ src/App.js
@@ -12,7 +12,19 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
+       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
         <button onClick={clickOnMe}>Click me</button>
         <span data-testid="count">{count}</span>
       </header>
